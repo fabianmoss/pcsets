a21cb9db9f061fd4912e3069aaaae83e62ce39c5
Fabian C. Moss <fabianmoss@gmail.com>
2023-08-02 19:56:33 +0200

    log different sets

--- src/components/PcSetAnalysis.vue
+++ src/components/PcSetAnalysis.vue
@@ -8,9 +8,26 @@
         class="form-control"
         type="text"
         v-model="pcSet"
-        @input="analyze"
+        @input="analyze(pcSet)"
+        @keydown.enter="logSet(pcSet)"
         placeholder="Enter pitch class set, e.g., 047 or 26E"
       />
+    </div>
+    <!-- Display the list of logged sets -->
+    <div class="mt-4">
+      <h3>Logged Sets:</h3>
+      <ul class="list-group">
+        <li
+          v-for="set in loggedSets"
+          :key="set"
+          @click="selectSet(set)"
+          class="list-group-item"
+          :class="{ 'active': set === selectedSet }"
+          style="cursor: pointer"
+        >
+          {{ set }}
+        </li>
+      </ul>
     </div>
     <div v-if="analysisResult" class="mt-4">
       <p><strong>Normal Form:</strong> {{ analysisResult.normalForm }}</p>
@@ -29,53 +46,73 @@ export default {
     return {
       pcSet: '',
       analysisResult: null,
+      loggedSets: [],
+      selectedSet: '',
     };
   },
   methods: {
-    analyze() {
-      const notes = this.pcSet.trim().toUpperCase();
-      const pitchClasses = notes.split('').map((note) => {
-        if (note === 'T') return 10;
-        if (note === 'E') return 11;
-        return parseInt(note);
-      });
-
-      // Sort the pitch classes in ascending order
-      pitchClasses.sort((a, b) => a - b);
-
-      // Calculate the normal form (transposition to the lowest pitch class)
-      const transposition = pitchClasses[0];
-      const normalForm = pitchClasses.map((pc) => (pc - transposition + 12) % 12);
-
-      // Calculate the prime form (rotation to the most compact form)
-      const primeForm = [...normalForm].sort((a, b) => a - b);
-
-      // Calculate the Forte number (unique identifier for set classes)
-      const forteNumber = primeForm.join('');
-
-      // Calculate the set class (removing duplicate forms)
-      const setClass = Array.from(new Set(primeForm)).join('');
-
-      // Calculate the number of symmetries
-      const numSymmetries = primeForm.length === 1 ? 1 : 12 / primeForm.length;
-
-      // Calculate the cardinality (number of pitch classes in the set)
-      const cardinality = primeForm.length;
-
-      // Set the analysis results
-      this.analysisResult = {
-        normalForm: normalForm.join(''),
-        primeForm: primeForm.join(''),
-        forteNumber,
-        setClass,
-        numSymmetries,
-        cardinality,
+    analyze(set) {
+      const analysisResult = {
+        normalForm: this.getNormalForm(set),
+        primeForm: this.getPrimeForm(set),
+        forteNumber: this.getForteNumber(set),
+        setClass: this.getSetClass(set),
+        numSymmetries: this.getNumSymmetries(set),
+        cardinality: set.length,
       };
+      this.analysisResult = analysisResult;
+    },
+    logSet() {
+      const enteredSet = this.pcSet.trim().toUpperCase();
+      if (enteredSet !== '' && !this.loggedSets.includes(enteredSet)) {
+        this.loggedSets.push(enteredSet);
+      }
+      this.pcSet = '';
+      this.selectedSet = enteredSet;
+    },
+    selectSet(set) {
+      this.selectedSet = set;
+      this.analyze(set); // Call the analyze method when a logged set is selected
+    },
+    // Your analysis functions here, working directly with the input string set
+    getNormalForm(set) {
+      // Placeholder example: Return the set itself as the normal form
+      return set;
+    },
+    getPrimeForm(set) {
+      // Placeholder example: Return the set itself as the prime form
+      return set;
+    },
+    getForteNumber(set) {
+      // Placeholder example: Return '0' as the Forte number
+      return set;
+    },
+    getSetClass(set) {
+      // Placeholder example: Return 'Unknown' as the set class
+      return set;
+    },
+    getNumSymmetries(set) {
+      // Placeholder example: Return '0' as the number of symmetries
+      return set;
     },
   },
 };
 </script>
 
 <style>
-/* No custom styles needed at this point */
+.list-group {
+  margin-top: 1rem;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  padding: 0;
+}
+
+.list-group-item {
+  cursor: pointer;
+  border-bottom: 1px solid #ccc;
+}
+
+.list-group-item:last-child {
+  border-bottom: none;
+}
 </style>
